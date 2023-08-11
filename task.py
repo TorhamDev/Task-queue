d3bbd41aae96c52efac5f8ba666dc7b0e490bcf1
Torham <TorhamDev@gmail.com>
2023-08-11 06:24:31 +0330

    Add sleep

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 from redis import Redis
+from time import sleep
 
 redis_db = Redis("localhost", 6379, 0)
 DEFAULT_QUEUE = "default"
@@ -7,3 +8,4 @@ while True:
     task = redis_db.rpop(DEFAULT_QUEUE)
     if task != None:
         print(f"Doing Task {task.decode()} ...")
+        sleep(10)
